cc00bfb6723c89d69671c77269470356b53604dc
James Lopez <jimmylop2003@yahoo.com>
2015-04-24 12:06:40 -0700

    changed

--- app/python1.py
+++ app/python1.py
@@ -1,1 +1,21 @@
-#import blah
+#!/usr/bin/env python
+
+import sys
+
+from cobra.mit.session import LoginSession 
+from cobra.mit.access import MoDirectory 
+from cobra.mit.request import ConfigRequest 
+from cobra.model.fv import Tenant
+from cobra.internal.codec.xmlcodec import toXMLStr
+
+def apic_login(hostname, username, password): 
+    url = "http://" + hostname
+    sess = LoginSession(url, username, password) modir = MoDirectory(sess)
+    try:
+        modir.login()
+    except:
+        print 'Login error'
+        exit(1)
+    return modir
+pass
+
